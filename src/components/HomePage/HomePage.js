5f17f2b76ce3800c310541327fbcd72f946d520b
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-30 19:46:58 +0600

    added hero section

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import img from '../../images/vector.png';
+import { FaFacebook, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
 
 const HomePage = () => {
   return (
-    <div className='px-10'>
-      <h1 className='text-3xl text-center'>This is my home page</h1>
-      <p className='text-gray-500'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad quisquam consequatur quo facilis ducimus vitae et temporibus deserunt sapiente, cum ratione nisi minima odit. Voluptas praesentium temporibus autem perspiciatis veritatis molestiae quos saepe voluptatem excepturi odit? Ratione iste amet ad, ea dolorem, repudiandae molestiae animi modi blanditiis corrupti consectetur voluptatem consequatur doloremque voluptatum nemo, voluptate id aspernatur maxime aliquid fugiat odit quidem recusandae sint corporis? Maxime expedita quaerat rerum nam quibusdam dignissimos sed minus vero ipsam, eveniet molestiae eaque repudiandae veniam, soluta doloribus autem fugit ad aut tenetur quod dolor ratione laboriosam. Illo repellat fuga repudiandae recusandae dignissimos similique iusto deleniti labore reprehenderit illum culpa sed nobis laboriosam vitae dolore, atque, tenetur magni obcaecati? Mollitia rem officiis consectetur architecto modi, quisquam quis excepturi minus, recusandae vitae porro odit, impedit minima in quasi doloribus illum corrupti? Soluta ducimus tempora mollitia iure suscipit iste eveniet nisi dolorem, dolore pariatur labore exercitationem temporibus id. Velit dolorem soluta sint quibusdam illum saepe, rerum eius alias, error provident omnis, neque maiores sed voluptates iure unde! Adipisci a voluptatibus minima nostrum autem eius, alias sit quisquam veritatis. Praesentium, enim eaque libero perspiciatis error ipsam sunt itaque consequatur sint quas delectus accusantium corrupti officiis saepe impedit earum!</p>
-    </div>
+      <section className='hero mt-10'>
+            <div className="container">
+              <div className="row grid gap-5 md:grid-cols-2 justify-between items-center">
+                <div className="content-left px-10 md:px-20">
+                  <div className="hero-title border-l-4 border-orange-400 pl-3">
+                  <h2 className='text-4xl font-mono text-gray-700 font-bold'>Before they sold out readymade gluten </h2>
+                  </div>
+                  <p className='text-gray-500 py-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta quasi deleniti culpa inventore, dolorem, ipsam architecto veritatis blanditiis voluptatibus facilis est tenetur. Ut numquam minus ex fuga tempore et laborum!</p>
+                  <button className='btn bg-orange-400 px-4 py-2 rounded text-2xl hover:bg-indigo-500 transition duration-75'>Download CV</button>
+                  <button className='btn bg-indigo-300 px-4 py-2 rounded ml-3 text-2xl hover:bg-orange-500'>Hire me</button>
+                  <div className="social-icon my-5 pl-10 flex">
+                      <a className='p-1 mr-2 bg-blue-700 text-white text-xl rounded-full' href="/facebook"> 
+                          <FaFacebook /> 
+                      </a>
+                      <a className='p-1 mr-2 bg-blue-400 text-white text-xl rounded-full' href="/facebook"> 
+                          <FaTwitter /> 
+                      </a>
+                      <a className='p-1 bg-blue-500 text-white text-xl rounded-full' href="/facebook"> 
+                          <FaLinkedinIn /> 
+                      </a>
+                  </div>
+                </div>
+                <div className="content-right">
+                  <img src= {img} alt="" />
+                </div>
+              </div>
+            </div>
+          </section>
   );
 };
 
--- src/components/MealDetails/MealDetails.js
+++ src/components/MealDetails/MealDetails.js
@@ -13,19 +13,19 @@ const MealDetails = () => {
   },[mealId])
 
   return (
-    <div className='text-center'>
-      <h2 className='text-5xl text-center py-5'>Food Id: {mealId} </h2>
+    <div className='text-center bg-gradient-to-r from-gray-500 via-green-500 to-gray-500'>
+      <h2 className='text-3xl text-center py-5 text-violet-600 font-bold'>Food Id: {mealId} </h2>
 
-          <img className='w-[400px] mx-auto rounded-2xl' src= {details.strMealThumb} alt="" />
-        <h2 className='text-2xl font-mono font-bold'> Name: <span className='text-indigo-500'>{details.strMeal}</span> </h2>
+          <img className='w-[600px] h-[350px] mx-auto rounded-2xl' src= {details.strMealThumb} alt="" />
+        <h2 className='text-2xl font-mono '> Name: <span className='text-gray-700'>{details.strMeal}</span> </h2>
 
-        <h2 className='text-2xl font-mono font-bold'> Area: <span className='text-indigo-500'>{details.strArea}</span> </h2>
+        <h2 className='text-2xl font-mono'> Area: <span className='text-gray-700'>{details.strArea}</span> </h2>
 
-        <h2 className='text-2xl font-mono font-bold'> Category: <span className='text-indigo-500'>{details.strCategory}</span> </h2>
+        <h2 className='text-2xl font-mono'> Category: <span className='text-gray-700'>{details.strCategory}</span> </h2>
 
-        <h2 className='text-2xl font-mono font-bold'> Ingredient: <span className='text-indigo-500'>{details.strIngredient1}, {details.strIngredient2},{details.strIngredient3},{details.strIngredient4},{details.strIngredient5},{details.strIngredient6},{details.strIngredient7},{details.strIngredient8}</span> </h2>
+        <h2 className='text-2xl font-mono'> Ingredient: <span className='text-gray-700'>{details.strIngredient1}, {details.strIngredient2},{details.strIngredient3},{details.strIngredient4},{details.strIngredient5},{details.strIngredient6},{details.strIngredient7},{details.strIngredient8}</span> </h2>
 
-        <h2 className='text-2xl font-bold pt-5'> Description: <span className='text-gray-500 font-normal text-xl'>{details.strInstructions}</span> </h2>
+        <h2 className='text-2xl py-5'> Description: <span className='text-gray-300 font-normal text-xl'>{details.strInstructions}</span> </h2>
     </div>
   );
 };
