b6c66a8440f8c261d137f6f1bd36212eb3c1706d
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-30 20:07:49 +0600

    added contact section

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -2,9 +2,44 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
-      <h2 className='text-3xl text-center'>This is my contact page</h2>
+
+  <div className="bg-gradient-to-b from-purple-600 to-indigo-700 h-96 w-full">
+    <div className="w-full flex items-center justify-center ">
+        <div className="absolute top-40 bg-white dark:bg-gray-800 shadow rounded py-12 lg:px-28 px-8">
+            <p className="md:text-3xl text-xl font-bold leading-7 text-center text-gray-700 dark:text-white">Let’s chat and get a quote!</p>
+            <div className="md:flex items-center mt-12">
+                <div className="md:w-72 flex flex-col">
+                    <label className="text-base font-semibold leading-none text-gray-800 dark:text-white">Name</label>
+                    <input tabindex="0" arial-label="Please input name" type="name" className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100" placeholder="Please input  name" />
+                </div>
+                <div className="md:w-72 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label className="text-base font-semibold leading-none text-gray-800 dark:text-white">Email Address</label>
+                    <input tabindex="0" arial-label="Please input email address" type="name" className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100" placeholder="Please input email address" />
+                </div>
+            </div>
+            <div className="md:flex items-center mt-8">
+                <div className="md:w-72 flex flex-col">
+                    <label className="text-base font-semibold leading-none text-gray-800 dark:text-white">Company name</label>
+                    <input tabindex="0" role="input" arial-label="Please input company name" type="name" className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100" placeholder="Please input company name" />
+                </div>
+                <div className="md:w-72 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label className="text-base font-semibold leading-none text-gray-800 dark:text-white">Country</label>
+                    <input tabindex="0" arial-label="Please input country name" type="name" className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100" placeholder="Please input country name" />
+                </div>
+            </div>
+            <div>
+                <div className="w-full flex flex-col mt-8">
+                    <label className="text-base font-semibold leading-none text-gray-800 dark:text-white">Message</label>
+                    <textarea tabindex="0" aria-label="leave a message" role="textbox" type="name" className="h-36 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100 resize-none"></textarea>
+                </div>
+            </div>
+            <p className="text-xs leading-3 text-gray-600 dark:text-gray-200 mt-4">By clicking submit you agree to our terms of service, privacy policy and how we use data as stated</p>
+            <div className="flex items-center justify-center w-full">
+                <button className="mt-9 text-base font-semibold leading-none text-white py-4 px-10 bg-indigo-700 rounded hover:bg-indigo-600 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none">SUBMIT</button>
+            </div>
+        </div>
     </div>
+</div>
   );
 };
 
--- src/components/MealDetails/MealDetails.js
+++ src/components/MealDetails/MealDetails.js
@@ -16,7 +16,7 @@ const MealDetails = () => {
     <div className='text-center bg-gradient-to-r from-gray-500 via-green-500 to-gray-500'>
       <h2 className='text-3xl text-center py-5 text-violet-600 font-bold'>Food Id: {mealId} </h2>
 
-          <img className='w-[600px] h-[350px] mx-auto rounded-2xl' src= {details.strMealThumb} alt="" />
+          <img className='w-[600px] h-[350px] mx-auto rounded-2xl md:rounded-3xl p-10 md:p-0' src= {details.strMealThumb} alt="" />
         <h2 className='text-2xl font-mono '> Name: <span className='text-gray-700'>{details.strMeal}</span> </h2>
 
         <h2 className='text-2xl font-mono'> Area: <span className='text-gray-700'>{details.strArea}</span> </h2>
@@ -25,7 +25,7 @@ const MealDetails = () => {
 
         <h2 className='text-2xl font-mono'> Ingredient: <span className='text-gray-700'>{details.strIngredient1}, {details.strIngredient2},{details.strIngredient3},{details.strIngredient4},{details.strIngredient5},{details.strIngredient6},{details.strIngredient7},{details.strIngredient8}</span> </h2>
 
-        <h2 className='text-2xl py-5'> Description: <span className='text-gray-300 font-normal text-xl'>{details.strInstructions}</span> </h2>
+        <h2 className='text-2xl py-5 px-5 text-justify'> Description: <span className='text-gray-300 font-normal text-xl'>{details.strInstructions}</span> </h2>
     </div>
   );
 };
