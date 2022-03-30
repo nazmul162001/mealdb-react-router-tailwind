5a4876ecae9ba04f107c4479b7f8d327992d6c5f
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-30 12:49:08 +0600

    responsive for mobile & tablet device

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,16 +5,16 @@ const Navbar = (props) => {
   const {setSearchText} = props
 
   return (
-    <nav className='h-[80px] bg-indigo-400 flex justify-between items-center px-10'>
-      <div className="logo">
+    <nav className='h-[150px] md:h-[80px] bg-indigo-400 flex flex-col md:flex-row justify-center md:justify-between items-center px-10'>
+      <div className="logo md:order-1">
       <Link to="/" className='text-3xl font-mono font-bold'>Logo</Link>
       </div>
 
-      <div className="search-field">
+      <div className="search-field order-3 md:order-2">
         <input onChange={(e) => setSearchText(e.target.value)} className='md:w-[300px] h-[35px] bg-indigo-300 rounded-r-full rounded-l-full border-0 pr-3 pl-5 placeholder-gray-500  shadow-current shadow' placeholder='search your meal' type="search" name="" id="" />
       </div>
 
-      <div className="nav-menu">
+      <div className="nav-menu md:order-3">
         <Link className='px-2 text-white text-xl' to="/">Home</Link>
         <Link className='px-2 text-white text-xl' to="/meals">Meal</Link>
         <Link className='px-2 text-white text-xl' to="/contact">Contact</Link>
